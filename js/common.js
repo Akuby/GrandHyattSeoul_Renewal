bfc82febe40df40a5e23481d59d1ea1683cdba7d
Akuby <akuby1997@gmail.com>
2022-07-29 13:45:02 +0900

    rooms 1200px 조정

--- js/common.js
+++ js/common.js
@@ -4,3 +4,5 @@ $('.promo-slider ul li').each(function() {
 $('.rooms-slider li').each(function() {
   $(this).css({backgroundImage:`url(../assets/room-${$(this).index()+1}.png)`})
 })
+let roomsWidth = parseInt($('#rooms').css('width'))*48/100;
+$('#rooms svg line').attr('x2', roomsWidth + '%'); // 안됌
