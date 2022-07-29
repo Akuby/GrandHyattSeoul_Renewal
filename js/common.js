291beceb8daf1fbafcd8e907d6421c3ece71a5a5
Akuby <akuby1997@gmail.com>
2022-07-29 12:27:07 +0900

    rooms position 잡기

--- js/common.js
+++ js/common.js
@@ -1,3 +1,6 @@
 $('.promo-slider ul li').each(function() {
   $(this).css({backgroundImage:`url(../assets/offer-${$(this).index()+1}.png)`})
 })
+$('.rooms-slider li').each(function() {
+  $(this).css({backgroundImage:`url(../assets/room-${$(this).index()+1}.png)`})
+})
