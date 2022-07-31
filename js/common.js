e2e1bb7e54d2c4f8c316d3f6189ff0216a05bf50
Akuby <akuby1997@gmail.com>
2022-08-01 00:45:52 +0900

    wedding video 태그 전까지

--- js/common.js
+++ js/common.js
@@ -7,5 +7,9 @@ $('.rooms-slider li').each(function() {
 $('.dining-slider li').each(function() {
   $(this).css({backgroundImage:`url(../assets/dining-${$(this).index()+1}.png)`})
 })
+$('#facilities article').each(function() {
+  $(this).css({backgroundImage:`url(../assets/facilities-${$(this).index()+1}.png)`})
+})
 let rdWidth = parseInt($('#rooms').css('width'))*42/100;
 $('#rooms svg line').attr('x2', rdWidth);
+$('#dining svg line').attr('x1', rdWidth);
