1bbeeef728d12e5cc33c1313b11b3396c3d48706
Akuby <akuby1997@gmail.com>
2022-08-01 01:53:03 +0900

    footer 및 top 버튼 미완

--- js/common.js
+++ js/common.js
@@ -10,6 +10,9 @@ $('.dining-slider li').each(function() {
 $('#facilities article').each(function() {
   $(this).css({backgroundImage:`url(../assets/facilities-${$(this).index()+1}.png)`})
 })
+$('.posts a').each(function() {
+  $(this).css({backgroundImage:`url(../assets/sns-${$(this).parent().index()+1}.png)`})
+})
 let rdWidth = parseInt($('#rooms').css('width'))*42/100;
 $('#rooms svg line').attr('x2', rdWidth);
 $('#dining svg line').attr('x1', rdWidth);
