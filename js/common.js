68b3cc90f15e7b73fb369299f14e6ee044923f32
Akuby <akuby1997@gmail.com>
2022-08-04 02:11:12 +0900

    반응형 다이닝까지

--- js/common.js
+++ js/common.js
@@ -1,17 +1,27 @@
-$('.promo-slider ul li').each(function() {
-  $(this).css({backgroundImage:`url(../assets/offer-${$(this).index()+1}.png)`})
+$('.promo-slider ul li').each(function () {
+  $(this).css({
+    backgroundImage: `url(../assets/offer-${$(this).index()+1}.png)`
+  })
 })
-$('.rooms-slider li').each(function() {
-  $(this).css({backgroundImage:`url(../assets/room-${$(this).index()+1}.png)`})
+$('.rooms-slider li').each(function () {
+  $(this).css({
+    backgroundImage: `url(../assets/room-${$(this).index()+1}.png)`
+  })
 })
-$('.dining-slider li').each(function() {
-  $(this).css({backgroundImage:`url(../assets/dining-${$(this).index()+1}.png)`})
+$('.dining-slider li').each(function () {
+  $(this).css({
+    backgroundImage: `url(../assets/dining-${$(this).index()+1}.png)`
+  })
 })
-$('#facilities article').each(function() {
-  $(this).css({backgroundImage:`url(../assets/facilities-${$(this).index()+1}.png)`})
+$('#facilities article').each(function () {
+  $(this).css({
+    backgroundImage: `url(../assets/facilities-${$(this).index()+1}.png)`
+  })
 })
-$('.posts a').each(function() {
-  $(this).css({backgroundImage:`url(../assets/sns-${$(this).parent().index()+1}.png)`})
+$('.posts a').each(function () {
+  $(this).css({
+    backgroundImage: `url(../assets/sns-${$(this).parent().index()+1}.png)`
+  })
 })
 let rdWidth = parseInt($('#rooms').css('width')) * 42 / 100;
 $('#rooms svg line').attr('x2', rdWidth);
@@ -20,14 +30,22 @@ $('#dining svg line').attr('x1', rdWidth);
 
 // 스크롤을 내릴 때는 없다가, 위로 올리면 내려오도록!
 $(window).scroll(function () {
-  if ($(document).scrollTop() >= $('header').innerHeight()) {
-    $('#navWrap').css({position: 'fixed'});
-  } else if ($(document).scrollTop() < $('header').innerHeight()) {
-    $('#navWrap').css({position: 'absolute'})
-  }
+  if ($(window).innerWidth() > 599) {
+
+
+    if ($(document).scrollTop() >= $('header').innerHeight()) {
+      $('#navWrap').css({
+        position: 'fixed'
+      });
+    } else if ($(document).scrollTop() < $('header').innerHeight()) {
+      $('#navWrap').css({
+        position: 'absolute'
+      })
+    }
+  } 
 })
 
-$('#reserv_content .contWrap li').on('click', function() {
+$('#reserv_content .contWrap li').on('click', function () {
   $('#reserv_content .contWrap li').removeClass('selected')
   $(this).addClass('selected')
   $('#reserv_content .contWrap').prepend($(this))
